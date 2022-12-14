638b8d2bcf08e9c7c572f9a8f4a62f4ef78ae26c
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2022-12-14 22:41:15 +0300

    Программа анкетирования для сайта знакомств

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,15 +5,19 @@ class MainClass
 	public static void Main(string[] args)
 	{
 
-		string MyName = "Philipp";
-		byte MyAge = 29;
-		bool HaveIApet = false;
-		double MyShoeSize = 41.5;
+		Console.Write("Здравствуйте. Давайте найдем Вам пару по Знаку Зодиака. \nДа соединят Вас звезды!\n\n Введите Ваше имя: ");
+		var name = Console.ReadLine();
+		
+		Console.Write("Введите Ваш возраст: ");
+		var age = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("\n\nВас зовут {0}, какое красивое имя. \nИ самый хороший возраст для поиска пары, {1}. ", name, age);
 
-		Console.WriteLine("My name is " + MyName);
-		Console.WriteLine("MyAge " + MyAge);
-		Console.WriteLine("Do I have a pet? " + HaveIApet);
-		Console.WriteLine("My shoe size is " + MyShoeSize);
+		Console.Write("\nДавайте продолжим составлять идеальную анкету. \n\nКакая у Вас точная дата рождения (необходима для расчета кармического асцендента совместимости)? ");
+		var birthdate = Console.ReadLine();
+		Console.Write("\nВаш Знак Зодиака? ");
+		var zodiac = Console.ReadLine();
+
+		Console.WriteLine("\nТаким образом Ваша анкета выглядит так: \nИмя: \t{0} \nВозраст: \t{1} лет \nДата Вашего рождения: \t{2} \nЗнак Зодиака:\t {3}\n Удачных поисков!",name, age, birthdate, zodiac);
 
 	}
 }
