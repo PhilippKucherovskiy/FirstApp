e59dd4242ac230590a27f18dd337ecaa9c712c00
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-01-01 15:28:39 +0300

    Добавил приветствие Урусхану.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -12,12 +12,14 @@ class MainClass
 		var age = Convert.ToInt32(Console.ReadLine());
 		Console.WriteLine("\n\nВас зовут {0}, какое красивое имя. \nИ самый хороший возраст для поиска пары, {1}. ", name, age);
 
-		Console.Write("\nДавайте продолжим составлять идеальную анкету. \n\nКакая у Вас точная дата рождения (необходима для расчета кармического асцендента совместимости)? ");
+		Console.Write("\nДавайте продолжим составлять идеальную анкету. \n\nКакая у Вас точная дата рождения (необходима для расчета асцендента совместимости)? ");
 		var birthdate = Console.ReadLine();
 		Console.Write("\nВаш Знак Зодиака? ");
 		var zodiac = Console.ReadLine();
 
 		Console.WriteLine("\nТаким образом Ваша анкета выглядит так: \nИмя: \t{0} \nВозраст: \t{1} лет \nДата Вашего рождения: \t{2} \nЗнак Зодиака:\t {3}\n Удачных поисков!",name, age, birthdate, zodiac);
+
+		Console.WriteLine("Привет, Урусхан!");
 		Console.ReadKey();
 	}
 }
